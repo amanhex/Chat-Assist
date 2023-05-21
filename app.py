f6734e99c2df92f2a65a74456e2e9edb334cfb39
Amanhex <aamankumar4958@gmail.com>
2023-05-21 18:12:19 +0530

    update app.py

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.tokenize import word_tokenize
 import string
-import nltk
 import json
 
 app = Flask(__name__)
-
-nltk.download('punkt')
 
 def preprocess(text):
     # Tokenize the text
@@ -29,23 +26,17 @@ def load_training_data():
         training_data = [(qa['question'], qa['answer']) for qa in data]
     return training_data
 
-def train_chatbot(training_data):
-    # Preprocess training data
-    preprocessed_data = [preprocess(question) for question, _ in training_data]
+# Preprocess training data
+training_data = load_training_data()
+preprocessed_data = [preprocess(question) for question, _ in training_data]
 
-    # Initialize the vectorizer
-    vectorizer = TfidfVectorizer(tokenizer=preprocess)
+# Initialize the vectorizer
+vectorizer = TfidfVectorizer(tokenizer=preprocess)
 
-    # Fit the vectorizer on the preprocessed training data
-    query_vector = vectorizer.fit_transform(preprocessed_data)
-
-    return vectorizer, query_vector
+# Fit the vectorizer on the preprocessed training data
+query_vector = vectorizer.fit_transform(preprocessed_data)
 
 def get_chatbot_response(user_input, threshold=0.2):
-    vectorizer = app.config['vectorizer']
-    query_vector = app.config['query_vector']
-    training_data = app.config['training_data']
-
     preprocessed_input = preprocess(user_input)
     query_vector_input = vectorizer.transform([preprocessed_input])  # Transform the preprocessed input
     similarities = cosine_similarity(query_vector_input, query_vector)  # Compute cosine similarities
@@ -69,9 +60,4 @@ def get_response():
     return jsonify({'response': responses[0], 'alternatives': responses[1:]})
 
 if __name__ == '__main__':
-    training_data = load_training_data()
-    vectorizer, query_vector = train_chatbot(training_data)
-    app.config['vectorizer'] = vectorizer
-    app.config['query_vector'] = query_vector
-    app.config['training_data'] = training_data
     app.run(debug=True)
